9d3e546d1cc037c9625a9570234f9a6bb5c82a9e
jeec1998 <enriquezjarold4@gmail.com>
2024-05-18 17:59:20 -0500

    commit

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,16 +1,63 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
+import {type HydratedDocument} from 'mongoose';
 
-@Schema()
-export class User extends Document {
-  @Prop({ required: true })
-  name: string;
+export type UserDocument = HydratedDocument<User>;
 
-  @Prop()
+@Schema({timestamps: true})
+export class User {
+  @Prop({ type: String})
+  userId: string;
+
+  @Prop({ type: String, unique: true })
   email: string;
 
-  @Prop()
+  @Prop({ type: Boolean, required: true })
+  acceptTerms: boolean;
+
+  @Prop({ type: Boolean })
+  acceptContact: boolean;
+
+  @Prop({ type: Date })
+  expirationDate: Date;
+
+  @Prop({ type: String })
+  firstName: string;
+
+  @Prop({ type: String })
+  lastName: string;
+
+  @Prop({ type: Boolean, default: false})
+  isValidSubscription: boolean;
+
+  @Prop({ type: String })
+  phoneNumber: string;
+
+  @Prop({ type: String })
+  photoURL: string;
+
+  @Prop({ type: Object })
+  school: Record<string, any>;
+
+  @Prop({ type: String, required: true })
+  stripeId: string;
+
+  @Prop({ type: Object })
+  subjects: Record<string, any>;
+
+  @Prop({ type: Boolean, default: true })
+  enableComment: boolean;
+
+  @Prop({ type: String, required: true })
   password: string;
+
+  @Prop({ type: String })
+  resetPasswordToken: string;
+
+  @Prop({type: Date, default: null})
+  lastLoginAt: Date;
+
+  @Prop({type: Date})
+  createdAt: Date;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
