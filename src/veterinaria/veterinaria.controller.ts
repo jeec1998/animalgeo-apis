bdac12932352488b61198ee3272cad4d46cbc2cb
jeec1998 <enriquezjarold4@gmail.com>
2024-07-19 23:05:56 -0500

    feat:d

--- src/veterinaria/veterinaria.controller.ts
+++ src/veterinaria/veterinaria.controller.ts
@@ -10,21 +10,63 @@ import {
   Req,
   Query,
   ParseFloatPipe,
+  UploadedFiles,
+  UseInterceptors,
 } from '@nestjs/common';
 import { VeterinariaService } from './veterinaria.service';
 import { CreateVeterinariaDto } from './dto/create-veterinaria.dto';
 import { UpdateVeterinariaDto } from './dto/update-veterinaria.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { ApiBearerAuth } from '@nestjs/swagger';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('veterinaria')
 export class VeterinariaController {
   constructor(private readonly veterinariaService: VeterinariaService) {}
+
   @UseGuards(AuthGuard)
   @ApiBearerAuth()
   @Post()
-  create(@Req() req, @Body() createVeterinariaDto: CreateVeterinariaDto) {
-    return this.veterinariaService.create(createVeterinariaDto, req.user._id);
+  @UseInterceptors(FilesInterceptor('files', 2, {
+    storage: diskStorage({
+      destination: './uploads',
+      filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, `${uniqueSuffix}${extname(file.originalname)}`);
+      },
+    }),
+    fileFilter: (req, file, cb) => {
+      if (file.mimetype.match(/\/(jpg|jpeg|png|pdf)$/)) {
+        cb(null, true);
+      } else {
+        cb(new Error('Invalid file type, only JPG, JPEG, PNG and PDF is allowed!'), false);
+      }
+    },
+  }))
+  async create(
+    @UploadedFiles() files: Express.Multer.File[],
+    @Body() createVeterinariaDto: CreateVeterinariaDto,
+    @Req() req,
+  ) {
+    const userId = req.user._id;
+    const host = req.get('host');
+    const protocol = req.protocol;
+    
+    const imagVetFile = files.find(file => file.mimetype.includes('image'));
+    const certificatePdfFile = files.find(file => file.mimetype.includes('pdf'));
+
+    const imagVet = imagVetFile ? `${protocol}://${host}/uploads/${imagVetFile.filename}` : '';
+    const certificatePdf = certificatePdfFile ? `${protocol}://${host}/uploads/${certificatePdfFile.filename}` : '';
+    
+    const createVeterinariaData = {
+      ...createVeterinariaDto,
+      imagVet,
+      certificatePdf,
+      userId,
+    };
+    return this.veterinariaService.create(createVeterinariaData, userId);
   }
 
   @UseGuards(AuthGuard)
